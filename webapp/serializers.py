52a22d3b2b530ef34b9e27e305e24334a0e68dc2
Adewole Abidemi <adewolebidemi1998@gmail.com>
2022-08-27 23:02:17 +0100

    first commit

---
+++ webapp/serializers.py
@@ -0,0 +1,11 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Employees
+
+
+class EmployeesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Employees
+        # fields= ('firstname', 'lastname')
+        fields = '__all__'
---
+++ webapp/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path
+# from .views import webappList
+
+
+# urlpatterns = [
+#     path('', webappList.as_view(), name='webapp')
+# ]
